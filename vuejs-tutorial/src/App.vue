38b7182ddf504d57ea5a573cd25f49231e1a3d91
Késsia Castro <kessiacastro@gmail.com>
2017-12-10 17:24:01 -0300

    configurando bulma

--- vuejs-tutorial/src/App.vue
+++ vuejs-tutorial/src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="app">
+     <section class="hero is-primary is-bold is-fullheight">
+      <!-- Hero head: will stick at the top -->
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                VueJs Tutorial 1.0
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
 
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+        </div>
+      </div>
+
+      <!-- Hero footer: will stick at the bottom -->
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered">
+            <p>
+              <strong>VueJS Tutorial</strong> by <a href="http://github.com/kessiacastro">Késsia Castro</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -16,30 +46,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
